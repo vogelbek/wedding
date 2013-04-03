c10805a1959f026738b6f6f15becd5e091b1c30f
Adam Hendricksen <adam.hendricksen@gmail.com>
2013-04-03 15:26:54 -0700

    New rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wedding::Application.config.secret_token = 'a06314758d30e88417200795f79f145a14fb461d4495ceab2fa455611329d0a92d4c44cd23a2d3ca818347f0d1c05a5eaca0a99cf86b91e915906b5f860cc6da'
