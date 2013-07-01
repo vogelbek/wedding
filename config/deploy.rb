b723608fb808ed9c8645fe0874b15cd67b86ca7b
Renée De Voursney <renee@nird.us>
2013-07-01 18:35:20 -0400

    update the rvm cap verison

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 require "rvm/capistrano"                  # Load RVM's capistrano plugin.
 set :rvm_type, :system
-set :rvm_ruby_string, 'ruby-1.9.3-p362@wedding'        # Or whatever env you want it to run in.
+set :rvm_ruby_string, 'ruby-1.9.3-head@wedding'        # Or whatever env you want it to run in.
 
 # Bundler tasks
 require 'bundler/capistrano'
